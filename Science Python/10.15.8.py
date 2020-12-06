595f39199ce21e8fe078dc9d7220ba1f413b83cc
Neil <wangwrg@gmail.com>
2020-12-06 22:22:17 +1300

    new files

--- Science Python/10.15.8.py
+++ Science Python/10.15.8.py
@@ -1,1 +1,24 @@
-def 
+from random import randrange
+
+
+def compare(t):
+    length = len(t)-1
+    count = 0
+    for i in range(length):
+        if t[i] == t[i+1]:
+            count += 1
+            i += 2
+        else:
+            i += 1
+        if count == 2:
+            return True
+
+
+def birth_days():
+    bday_list = []
+    for i in range(23):
+        bday = randrange(1,365)
+        bday_list += [bday]
+    compare(bday_list)
+
+print(birth_days())
